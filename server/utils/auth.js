a4ec7cd525fbaddc6136197dea62467cc416b7c1
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-06 15:43:54 -0500

    add default JWT secret key, remove REST api functionality

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,29 +4,27 @@ const jwt = require('jsonwebtoken');
 require('dot-env');
 
 // set token secret and expiration date
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET || 'test secret phrase';
 const expiration = '2h';
 
 module.exports = {
-    authMiddleware: function (req, res, next) {
+    authMiddleware: function ({req}) {
         let token = req.query.token || req.headers.authorization;  // allows token to be sent via req.query or headers
 
         if (req.headers.authorization)  // In the HTTP header, shave off the word 'Bearer' at the beginning of the value
             token = token.split(' ').pop().trim();
 
         if (!token)
-            return res.status(400).json({message: 'You have no token!'});
+            return req;
 
-        // verify token and get user data out of it
         try{
             const {data} = jwt.verify(token, secret, {maxAge: expiration});
             req.user = data;
         }catch{
-            console.log('Invalid token');
-            return res.status(400).json({message: 'Invalid token'});
+            console.error('Invalid token');
         }
 
-        next();  // send to next middleware / endpoint
+        return req;
     },
 
     signToken: function ({username, email, _id}){
