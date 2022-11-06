07e9ac499eded0ebdba8deb09eab52b4babb0dff
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-06 15:53:05 -0500

    adjust User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
-const { Schema, model } = require('mongoose');
+
+const {Schema, model} = require('mongoose');
 const bcrypt = require('bcrypt');
 
 // import schema from Book.js
@@ -10,12 +11,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
+      trim: true
     },
     email: {
       type: String,
       required: true,
       unique: true,
       match: [/.+@.+\..+/, 'Must use a valid email address'],
+      trim: true
     },
     password: {
       type: String,
@@ -43,8 +46,8 @@ userSchema.pre('save', async function (next) {
 });
 
 // custom method to compare and validate password for logging in
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+userSchema.methods.isCorrectPassword = async function(password){
+  return await bcrypt.compare(password, this.password);
 };
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
